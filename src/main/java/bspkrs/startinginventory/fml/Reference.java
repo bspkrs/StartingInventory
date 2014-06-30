ef6a945f41450bddb47eda9649b42f960f0c7c3f
bspkrs <bspkrs@gmail.com>
2014-06-30 17:18:16 -0400

    removed unnecessary field

--- src/main/java/bspkrs/startinginventory/fml/Reference.java
+++ src/main/java/bspkrs/startinginventory/fml/Reference.java
@@ -1,13 +1,9 @@
 package bspkrs.startinginventory.fml;
 
-import bspkrs.util.config.Configuration;
-
 public class Reference
 {
-    public static final String  MODID        = "StartingInventory";
-    public static final String  NAME         = "StartingInventory";
-    public static final String  PROXY_COMMON = "bspkrs.startinginventory.fml.CommonProxy";
-    public static final String  PROXY_CLIENT = "bspkrs.startinginventory.fml.ClientProxy";
-    
-    public static Configuration config       = null;
+    public static final String MODID        = "StartingInventory";
+    public static final String NAME         = "StartingInventory";
+    public static final String PROXY_COMMON = "bspkrs.startinginventory.fml.CommonProxy";
+    public static final String PROXY_CLIENT = "bspkrs.startinginventory.fml.ClientProxy";
 }
